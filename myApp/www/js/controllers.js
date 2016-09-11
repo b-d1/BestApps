ff1da40a13cc9a018a1344b82e326723570c720d
blag0j <dimovski.blagoj@students.finki.ukim.mk>
2016-09-11 18:07:05 +0200

    11-Sep-Changes & Updates

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -10,47 +10,98 @@ angular.module('starter.controllers', [])
   //});
 
   // Form data for the login modal
-  $scope.loginData = {};
+  // $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+  // // Create the login modal that we will use later
+  // $ionicModal.fromTemplateUrl('templates/login.html', {
+  //   scope: $scope
+  // }).then(function(modal) {
+  //   $scope.modal = modal;
+  // });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+  // // Triggered in the login modal to close it
+  // $scope.closeLogin = function() {
+  //   $scope.modal.hide();
+  // };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+  // // Open the login modal
+  // $scope.login = function() {
+  //   $scope.modal.show();
+  // };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+  // // Perform the login action when the user submits the login form
+  // $scope.doLogin = function() {
+  //   console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+  //   // Simulate a login delay. Remove this and replace with your login
+  //   // code if using a login system
+  //   $timeout(function() {
+  //     $scope.closeLogin();
+  //   }, 1000);
+  // };
 })
 
 .controller('singleAppCtl', function($scope, $stateParams, $ionicModal, bestAppsApi) {
 var vm = this;
 var id = $stateParams.id;
-console.log("Item id:", id);
 $scope.app = bestAppsApi.getApp(id);
+
+$scope.addColor = function(rating) {
+
+if(rating >= 6) {
+return "rt-good";
+}
+else if(rating >= 3 && rating < 6) {
+return "";
+}
+else {
+return "rt-bad";
+}
+
+}
+
+$ionicModal.fromTemplateUrl('my-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  // Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+
 })
 
 
 .controller('myCtrl', function($scope, bestAppsApi) {
   $scope.apps = bestAppsApi.getApps();
+
+  $scope.checkSubCategory = function(subCatSelect, subCategory) {
+console.log(subCatSelect + " " + subCategory);
+    if(subCatSelect === subCategory || subCatSelect === "All") {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+
 })
 
 
--- myApp/www/js/service.js
+++ myApp/www/js/service.js
@@ -1,17 +1,23 @@
 angular.module('starter.service', []).factory('bestAppsApi', function($filter) {
      var apps = [
-    { id: 1, title: 'Instagram' , category: 'Social Media', avatar: 'img/instagram.png', downloads: 100, rating: '4.5', stars: "✮ ✮ ✮ ✮ ✡"},
-    { id: 2, title: 'Snapchat' , category: 'Social Media',  avatar: 'img/snapchat.png',  downloads: 120, rating: '3', stars: "✮ ✮ ✮ ✮ ✮"},
-    { id: 3, title: 'PokemonGo' , category: 'Games',  avatar: 'img/pokemongo.png', downloads: 10, rating: '5', stars:"✮ ✮ ✮ ✮ ✮"},
-    { id: 4, title: 'TinyScanner' , category: 'Scanner',  avatar: 'img/tinyscanner.png', downloads: 1, rating:'0.5', stars:"✡ ✡ ✡ ✡ ✡"},
-    { id: 5, title: 'Can You Escape?' , category: 'Games',  avatar: 'img/canyouescape.png', downloads: 3, rating: '1', stars: "✮ ✡ ✡ ✡ ✡"},
-    { id: 6, title: 'Skype' ,  category: 'Social Media', avatar: 'img/skype.png',  downloads: 150, rating: '5', stars: "✮ ✮ ✮ ✮ ✮"},
-    { id: 7, title: 'Outlook' ,  category: 'E-mail', avatar: 'img/outlook.png', downloads: 90, rating: '4', stars:"✮ ✮ ✮ ✮ ✡"},
-    { id: 8, title: 'QuizUp' , category: 'Games',  avatar: 'img/quizup.png', downloads: 18, rating:'2', stars:"✮ ✮ ✡ ✡ ✡"},
-    { id: 9, title: 'Backgammon' , category: 'Games',  avatar: 'img/backgammon.png', downloads: 2, rating: '0.2', stars: " ✡ ✡ ✡ ✡ ✡"},
-    { id: 10, title: 'Learn SQL' , category: 'Educational',  avatar: 'img/learnsql.png',  downloads: 3, rating: '1', stars: "✮ ✡ ✡ ✡ ✡"},
-    { id: 11, title: 'Piano Tiles' ,  category: 'Games',  avatar: 'img/pianotiles.png', downloads: 105, rating: '5', stars:"✮ ✮ ✮ ✮ ✮"},
-    { id: 12, title: 'Photoshop Express' , category: 'Photo editor',  avatar: 'img/photoshop.png', downloads: 25, rating:'4.3', stars:"✮ ✮ ✮ ✮ ✡"}
+    { id: 1, title: 'Instagram' , mainCategory: 'Apps', subCategory: 'Entertainment', avatar: 'img/instagram.png', downloads: 100, rating: '4.5', stars: "✮ ✮ ✮ ✮ ✡",
+     bestAppsInfo: {usabilityDesc: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie, velit ut rutrum mattis, purus erat porttitor velit, nec accumsan tortor nulla vel lectus. ", usabilityRating: 7.5,
+                    uxDesc: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie, velit ut rutrum mattis, purus erat porttitor velit, nec accumsan tortor nulla vel lectus. ", uxRating: 9.2, 
+                    overallDesc: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie, velit ut rutrum mattis, purus erat porttitor velit, nec accumsan tortor nulla vel lectus. "},
+     appMetaData: {releasedBy: "Instagram", requires: "Varies with Device", lastUpdate: "September 8, 2016", 
+                    description: "Instagram is a simple way to capture and share the world’s moments. Follow your friends and family to see what they’re up to, and discover accounts from all over the world that are sharing things you love. "}
+    },
+    { id: 2, title: 'Snapchat' , mainCategory: 'Apps', subCategory: 'Entertainment',  avatar: 'img/snapchat.png',  downloads: 120, rating: '3', stars: "✮ ✮ ✮ ✮ ✮"},
+    { id: 3, title: 'PokemonGo' , mainCategory: 'Games',  subCategory: 'Adventure', avatar: 'img/pokemongo.png', downloads: 10, rating: '5', stars:"✮ ✮ ✮ ✮ ✮"},
+    { id: 4, title: 'TinyScanner' , mainCategory: 'Apps',  subCategory: 'Tools', avatar: 'img/tinyscanner.png', downloads: 1, rating:'0.5', stars:"✡ ✡ ✡ ✡ ✡"},
+    { id: 5, title: 'Can You Escape?' , mainCategory: 'Games',  subCategory: 'Adventure', avatar: 'img/canyouescape.png', downloads: 3, rating: '1', stars: "✮ ✡ ✡ ✡ ✡"},
+    { id: 6, title: 'Skype' ,  mainCategory: 'Apps', subCategory: 'Communication', avatar: 'img/skype.png',  downloads: 150, rating: '5', stars: "✮ ✮ ✮ ✮ ✮"},
+    { id: 7, title: 'Outlook' ,  mainCategory: 'Apps', subCategory: 'Communication', avatar: 'img/outlook.png', downloads: 90, rating: '4', stars:"✮ ✮ ✮ ✮ ✡"},
+    { id: 8, title: 'QuizUp' , mainCategory: 'Games',  subCategory: 'Brain & Puzzle', avatar: 'img/quizup.png', downloads: 18, rating:'2', stars:"✮ ✮ ✡ ✡ ✡"},
+    { id: 9, title: 'Backgammon' , mainCategory: 'Games',  subCategory: 'Adventure', avatar: 'img/backgammon.png', downloads: 2, rating: '0.2', stars: " ✡ ✡ ✡ ✡ ✡"},
+    { id: 10, title: 'Learn SQL' , mainCategory: 'Apps',  subCategory: 'Education', avatar: 'img/learnsql.png',  downloads: 3, rating: '1', stars: "✮ ✡ ✡ ✡ ✡"},
+    { id: 11, title: 'Piano Tiles' ,  mainCategory: 'Games',  subCategory: 'Music', avatar: 'img/pianotiles.png', downloads: 105, rating: '5', stars:"✮ ✮ ✮ ✮ ✮"},
+    { id: 12, title: 'Photoshop Express' , mainCategory: 'Apps', subCategory: 'Tools',  avatar: 'img/photoshop.png', downloads: 25, rating:'4.3', stars:"✮ ✮ ✮ ✮ ✡"}
   ];
 
   function getApps() {
